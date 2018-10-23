3d256a78df9c5e2126dd129de3c7ef70f987cb38
Sean Langton <slangton1@gmail.com>
2018-10-23 03:39:53 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,7 +11,7 @@ text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-text.match(/^[A-Z](.)+[.?!]$/)
+if text.match(/^[A-Z](.)+[.?!]$/) then true else false end
 end
 
 def valid_phone_number?(phone)
