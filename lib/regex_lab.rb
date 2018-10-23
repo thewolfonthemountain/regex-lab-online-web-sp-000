20d6be6dc85497c53ca1d1e3bcb6d1e9f0170506
Sean Langton <slangton1@gmail.com>
2018-10-23 03:05:24 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-!word.match(/\b[aeiou]/) 
+!word.match(/\b[aeiou]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -7,7 +7,7 @@ text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
