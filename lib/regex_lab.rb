cdec0ebe6a8241a04923c6e7a172de1d151a625f
Sean Langton <slangton1@gmail.com>
2018-10-23 03:32:59 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-if(word.match(/\b[aeiou]/) != nil) then true else false end
+if(word.match(/\b[aeiou]/).nil? then false else true end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
