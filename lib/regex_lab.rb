2f0d062084e9415773cbd938da22c6ebea37992e
Sean Langton <slangton1@gmail.com>
2018-10-23 03:39:24 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-if word.match(/\b[aeiouAEIOU]/) then true
+if word.match(/\b[aeiouAEIOU]/) then true else false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
