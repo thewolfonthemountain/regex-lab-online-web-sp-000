b95144cede3f5a7390c1c2dcc611f45a7e278c45
Sean Langton <slangton1@gmail.com>
2018-10-23 03:07:43 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-!word.match(/\b[aeiou]/)
+word.match(/\b[aeiou]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -11,7 +11,7 @@ text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+text.match(/[A-Z]\w+[.?!]/)
 end
 
 def valid_phone_number?(phone)
