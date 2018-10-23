f65d4da2b51c4401d9149ab20294841573732307
Sean Langton <slangton1@gmail.com>
2018-10-23 02:54:57 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-if word.match(/\b[aeiou]/) != nil then true else false end
+word.match(/\b[aeiou]/).empty?
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
